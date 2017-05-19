be9f049f394bad32a5bb24c2382a1f5663d8f50c
Alexey Berdnikov <aberdnikov@gmail.com>
2017-05-19 13:44:43 +0500

    fixed

--- src/boot/boot.php
+++ src/boot/boot.php
@@ -6,35 +6,23 @@
 //****************************************
 //  Главная
 //****************************************
-\Larakit\Route\Route::item('verstak')
-    ->setBaseUrl('/verstak')
-    ->setNamespace('Larakit\Verstak\Controllers')
-    ->put();
-\Larakit\Route\Route::item('verstak/js')
-    ->setBaseUrl('/verstak/js')
-    ->setController('VerstakJs')
-    ->setAction('js')
-    ->setNamespace('Larakit\Verstak\Controllers')
-    ->put();
+Route::get('/verstak', [
+    'uses'=> 'Larakit\Verstak\Controllers\VerstakController@index'
+])->name('verstak');
+Route::get('/verstak/js', [
+    'uses'=> 'Larakit\Verstak\Controllers\VerstakJsController@js'
+])->name('verstak/js');
 if('production' != App::environment()) {
-    \Larakit\Route\Route::item('verstak/download')
-        ->setBaseUrl('/verstak/download')
-        ->setNamespace('Larakit\Verstak\Controllers')
-        ->setController('VerstakDownload')
-        ->put();
+    Route::get('verstak/download', [
+        'uses'=> 'Larakit\Verstak\Controllers\VerstakDownloadController@index'
+    ])->name('verstak/download');
 }
-\Larakit\Route\Route::item('verstak/frame_block')
-    ->setBaseUrl('/verstak/frame-block-{block}')
-    ->setNamespace('Larakit\Verstak\Controllers')
-    ->setController('VerstakFrame')
-    ->setAction('frameBlock')
-    ->put();
-\Larakit\Route\Route::item('verstak/frame_page')
-    ->setBaseUrl('/verstak/frame-page-{page}')
-    ->setNamespace('Larakit\Verstak\Controllers')
-    ->setController('VerstakFrame')
-    ->setAction('framePage')
-    ->put();
+Route::get('/verstak/frame-block-{block}', [
+    'uses'=> 'Larakit\Verstak\Controllers\VerstakFrameController@frameBlock'
+])->name('verstak/frame_block');
+Route::get('/verstak/frame-page-{page}', [
+    'uses'=> 'Larakit\Verstak\Controllers\VerstakFrameController@framePage'
+])->name('verstak/frame_page');
 \Larakit\Boot::register_view_path(public_path(\Larakit\Verstak\VerstakManager::$prefix), 'lk-verstak');
 
 //РЕГИСТРАЦИЯ БЛОКОВ
